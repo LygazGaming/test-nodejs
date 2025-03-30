d6bf5a71b283b418baa9e0fffdee56b3cdc45ea1
Lygaz Gaming <longa4609@gmail.com>
2025-03-30 15:30:44 +0700

    Edit file index

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const userRoutes = require('./routes/userRoutes');
-
+const userRoutes = require("./routes/userRoutes");
 
 // Middleware để parse JSON
 app.use(express.json());
 
 // Kết nối MongoDB (chuyển đổi sang IPv4 nếu gặp lỗi IPv6)
-const mongoURI = 'mongodb://127.0.0.1:27017/demonodejs'; // Chuỗi kết nối MongoDB cục bộ
+const mongoURI = "mongodb://127.0.0.1:27017/demonodejs"; // Chuỗi kết nối MongoDB cục bộ
 
 mongoose
-  .connect(mongoURI, {
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+  .connect(mongoURI, {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-
-  app.use('/users', userRoutes);
-
-
-// Route cơ bản để kiểm tra server
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js + MongoDB!');
-});
+app.use("/users", userRoutes);
 
 // Lắng nghe server (hardcode port)
 const PORT = 3000; // Port cố định
